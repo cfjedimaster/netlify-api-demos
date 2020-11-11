76aae905a21c1d23839d65443399e3d8d81f879d
Raymond Camden <raymondcamden@gmail.com>
2020-11-11 09:48:35 -0600

    misc

--- deploy-stats/getDeployStats.js
+++ deploy-stats/getDeployStats.js
@@ -4,6 +4,11 @@ const fetch = require('node-fetch');
 const token = process.env.NETLIFY_TOKEN;
 const siteId = process.env.NETLIFY_SITEID;
 
+// simple function that given S seconds, returns either "s seconds" or "n.x minutes"
+function formatTime(s) {
+	if(s < 120) return `${s} seconds`;
+	return `${(s/60).toFixed(1)} minutes`;
+}
 
 (async () => {
     let endpoint = `https://api.netlify.com/api/v1/sites/${siteId}/deploys`;
@@ -18,8 +23,18 @@ const siteId = process.env.NETLIFY_SITEID;
 		return prev + cur.deploy_time;
 	},0);
 	let total = data.length;
-	// seconds
+
 	let avg = totalTime / total;
-	let mins = (avg / 60).toFixed(2);
-	console.log(`I got ${total} deploys for an average build time of ${mins} minutes.`);
+
+	let mintime = Number.MAX_SAFE_INTEGER;
+	let maxtime = Number.MIN_SAFE_INTEGER;
+	data.forEach(d => {
+		if(d.deploy_time) {
+			if(d.deploy_time > maxtime) maxtime = d.deploy_time;
+			if(d.deploy_time < mintime) mintime = d.deploy_time;
+		}
+	});
+
+	console.log(`I got ${total} deploys for an average build time of ${formatTime(avg)}. 
+The smallest time was ${formatTime(mintime)} and the longest was ${formatTime(maxtime)}.`);
 })()
